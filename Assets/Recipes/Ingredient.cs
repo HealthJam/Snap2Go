73c65c9d082b402001d303fe89638715bea84516
Scott Beck <scottbeck89@gmail.com>
2018-04-14 16:43:16 -0400

    Adding initial data for Recipes and Ingredients

---
+++ Assets/Recipes/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(menuName = "Recipe/Ingredient")]
+public class Ingredient : ScriptableObject
+{
+    public string IngredientName;
+
+    public GameObject prefab;
+
+    public Measurement unitType;
+}
