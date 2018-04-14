8d6635dad57d1d62ec25a76ee2f4f930dd567ce0
John Li <johnliglobal@gmail.com>
2018-04-14 17:34:17 -0400

    Added code for retrieving snap locations

---
+++ Assets/Scripts/Location/LocationProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LocationProvider : MonoBehaviour {
+
+    public List<SnapLocation> snapLocationList = new List<SnapLocation>();
+
+    // Use this for initialization
+    void Start()
+    {
+        TextAsset targetFile = Resources.Load<TextAsset>("snap_locations");
+        string snapLocationJson = targetFile.text;
+        IDictionary result = (IDictionary )MiniJSON.Json.Deserialize(snapLocationJson);
+        IList locationList = (IList)result["results"];
+        foreach (IDictionary location in locationList)
+        {
+            IDictionary attributes = location["attributes"] as IDictionary;
+            SnapLocation snapLocation = new SnapLocation(Convert.ToInt32(attributes["OBJECTID"]),
+                attributes["STORE_NAME"].ToString(),
+                Convert.ToDouble(attributes["longitude"]),
+                Convert.ToDouble(attributes["latitude"]),
+                attributes["ADDRESS"].ToString(),
+                attributes["ADDRESS2"].ToString(),
+                attributes["CITY"].ToString(),
+                attributes["STATE"].ToString(),
+                attributes["ZIP5"].ToString(),
+                attributes["County"].ToString());
+            snapLocationList.Add(snapLocation);
+        }
+    }
+}
---
+++ Assets/Scripts/Location/SnapLocation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SnapLocation {
+    public SnapLocation(int id, string storeName, double longitude, double latitude, string streetAddress, string addressTwo, string city, string state, string zip, string county)
+    {
+        this.id = id;
+        this.storeName = storeName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.streetAddress = streetAddress;
+        this.addressTwo = addressTwo;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.county = county;
+    }
+
+    public int id;
+    public string storeName;
+    public double longitude;
+    public double latitude;
+    public string streetAddress;
+    public string addressTwo;
+    public string city;
+    public string state;
+    public string zip;
+    public string county;
+}
