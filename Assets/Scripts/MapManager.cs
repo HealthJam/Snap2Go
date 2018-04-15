ba6c203260e19d34616757505cf58fc28fa8b8bf
Gamingod4u <mcman407@gmail.com>
2018-04-15 13:42:10 -0400

    Creating poi state

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Examples;
+public class MapManager : MonoBehaviour 
+{
+	public static MapManager instance;
+
+	public enum GameMode
+	{
+		EXPLORER,
+		PARENT, 
+		EXPO
+	};
+
+	public GameObject locationProvider;
+
+
+	private GameMode mode;
+	private SpawnOnMap map;
+
+
+	private bool isDiscoverable = true;
+	// Use this for initialization
+	void Start () 
+	{
+		instance = this;
+		SetNewMode (GameMode.EXPLORER);
+		map = GetComponent<SpawnOnMap> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		
+	}
+
+
+
+
+
+	public void SetNewMode (GameMode _mode)
+	{
+		mode = _mode;
+
+		if (mode == GameMode.EXPLORER) {
+
+			isDiscoverable = true;
+
+
+		}
+	}
+}
---
+++ Assets/Scripts/POI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mapbox.Unity.Utilities;
+
+public class POI : MonoBehaviour 
+{
+
+	private double lon;
+	private double lat;
+	private bool isDiscovered;
+
+
+
+	public enum POIState
+	{
+		UNDISCOVERED,
+		DISCOVERED, 
+		PARENTMODE
+	}
+
+	private POIState state;
+	private POIState lastState;
+
+	// Use this for initialization
+	void Init (double _lat, double _lon) {
+		// use this to place object VectorExtensions;
+		lat = _lat;
+		lon = _lon;
+
+		state = POIState.UNDISCOVERED;
+
+		lastState = state;
+
+		SetNewState (state);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+		
+	public void SetNewState (POIState newState)
+	{
+		// do things to clean last state 
+		switch (newState)
+		{
+		case POIState.UNDISCOVERED:
+			{}
+			break;
+		case POIState.DISCOVERED:
+			{}
+			break;
+		case POIState.PARENTMODE:
+			{}
+			break;
+		}
+		state = newState;
+	}
+
+	public void SetLocation(double _lat, double _lon)
+	{
+		
+	}
+}
