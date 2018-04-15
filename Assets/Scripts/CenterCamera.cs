98f69095a78e44982cb732265825f1318960dfb2
Gamingod4u <mcman407@gmail.com>
2018-04-14 22:33:19 -0400

    added center button

---
+++ Assets/Scripts/CenterCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CenterCamera : MonoBehaviour {
+
+	public void OnCenterButtonClicked()
+	{
+		if (transform.position.x != 0) 
+		{
+			transform.position = new Vector3 (0, transform.position.y, 0);
+		}
+	}
+}
